53548e5fb2d816f93bd87037bdf2432d2e81a6ae
ibrahimtamimi <ibrahimtamimi5@gmail.com>
2016-08-18 11:51:23 +0300

    autoComplete

---
+++ autocomplete/autocomplete.js
@@ -0,0 +1,36 @@
+// TODO: Replace examples and use TDD development by writing your own tests
+
+// These are some CW specific test methods available:
+//    Test.expect(boolean, [optional] message)
+//    Test.assertEquals(actual, expected, [optional] message)
+//    Test.assertSimilar(actual, expected, [optional] message)
+//    Test.assertNotEquals(actual, expected, [optional] message)
+
+// NodeJS assert is also automatically required for you.
+//    assert(true)
+//    assert.strictEqual({a: 1}, {a: 1})
+//    assert.deepEqual({a: [{b: 1}]}, {a: [{b: 1}]})
+
+// You can also use Chai (http://chaijs.com/) by requiring it yourself
+// var expect = require("chai").expect;
+// var assert = require("chai").assert;
+// require("chai").should();
+
+// describe("Solution", function(){
+//   it("should test for something", function(){
+//     Test.assertEquals("actual", "expected", "This is just an example of how you can write your own TDD tests");
+//   });
+// });
+
+//autocomplete('ai', ['airplane','airport','apple','ball']) = ['airplane','airport']
+
+
+function autocomplete(input, dictionary){
+var res = [];
+  for (var i = 0; i < dictionary.length; i++) {
+  	if(dictionary[i].indexOf(input) !== -1){
+  		res.push(dictionary[i]);
+  	}
+  }
+  return res;
+}
